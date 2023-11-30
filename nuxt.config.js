19f63a6ae7cb6b04e1c4d98928302461775886a4
attapat <attapat@dek-d.com>
2023-11-30 20:12:11 +0700

    update plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    {src:"~/plugins/longdo-map.js",mode: "client"}
+    {src:"~/plugins/longdo-map.js",mode: "client"} // work for client-side only
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
